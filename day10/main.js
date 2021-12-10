4da705abe1bc0a7f9d31486696fbfffdeb4b12bb
Mats Törnberg <mats.tornberg@tretton37.com>
2021-12-10 09:08:22 +0100

    fattar inte

--- day10/main.js
+++ day10/main.js
@@ -1,71 +1,149 @@
-const { seq, chars, lower, manhattan, stepper, numbers } = require('../common.js');
+const {
+    seq,
+    chars,
+    lower,
+    manhattan,
+    stepper,
+    numbers,
+    add,
+} = require("../common.js");
 
-const transform = (data) => data.split('\r\n').map(line => line.trim().split(''));
+const transform = (data) =>
+    data.split("\r\n").map((line) => line.trim().split(""));
 
 const parts = [
-    ['(', ')'],
-    ['[', ']'],
-    ['{', '}'],
-    ['<', '>'],
-]
+    ["(", ")"],
+    ["[", "]"],
+    ["{", "}"],
+    ["<", ">"],
+];
 
-const hasStartChar = (chr) => parts.some(([s])=>s===chr);
+const values = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137,
+};
 
-const isEndChar = (start) => parts.find(([s])=>s==start)[1];
+const hasStartChar = (chr) => parts.some(([s]) => s === chr);
+
+const endChar = (start) => parts.find(([s]) => s === start)[1];
 
 const removeIncorrect = (line) => {
     const len = line.length;
-    const last = line[len-1];
-    if (hasStartChar(last))
-    {
-        console.log('incomplete',line.join(''));
-        return false;
-    }
 
     let pos = 0,
         walked = [];
-        started = [];
+    (started = []), (char = undefined);
 
     const walk = () => {
-        if (pos<len) {
-            const char = line[pos++];
+        if (pos < len) {
+            char = line[pos++];
             if (hasStartChar(char)) {
                 started.push(char);
                 walked.push(char);
-                walk();
-            }
-            else if (isEndChar(started[started.length-1])) {
-                //console.log('found closing',started[started.length-1], char);
+                return walk();
+            } else if (endChar(started[started.length - 1]) === char) {
                 walked.push(char);
                 started.pop();
-                walk();
+                return walk();
+            } else {
+                return;
             }
-            
         }
-        console.log(pos,len,walked.join(''), started.join(''));
-    }
+    };
     walk();
-    return pos===len;
-    // console.log(pos, len);
 
-    // const pairs = parts.some(([s, e]) => {
-    //     return (line[0]===s) && (line[line.length-1]===e);
-    // });
-    // console.log('pairs',pairs);
-    // return pairs;//.some(a=>a);
-}
+    if (pos !== len) {
+        return values[char];
+    }
+    return 0;
+};
+
+const autoCompleteValues = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
+};
+
+const autoCorrect = (line) => {
+
+    let pos = 0;
+    let walked = [];
+    let started = [];
+    let corrected = [];
+    let added = 0;
+    let char = undefined;
+
+    //console.log("starting line", line);
+    const walk = () => {
+
+        if (started.length > 0 || pos <= line.length) {
+            char = line[pos];
+
+            //console.log('testing',char,pos, corrected.join(''), pos, line.length);
+            if (hasStartChar(char)) {
+                started.push(char);
+                walked.push(char);
+                pos++;
+                walk();
+            } else {
+                if (started.length === 0)
+                    return;
+
+                const shouldBe = endChar(started[started.length - 1]);
+                if (shouldBe === char) {
+                    walked.push(char);
+                    started.pop();
+                    pos++;
+                    walk();
+                } else {
+                    corrected.push(shouldBe);
+                    line.splice(pos, 0, shouldBe);
+
+                    added *= 5;
+                    added += autoCompleteValues[shouldBe];
+
+                    walk();
+                }
+            }
+        }
+    };
+    walk();
+
+    return added;
+};
 
 const part1 = (i) => {
-    const clean = i.filter(removeIncorrect);
-    //console.log(clean);
-    return 1;
+
+    return i.map(removeIncorrect).reduce(add, 0);
+};
+
+const countLowerAndHigher = (all) => (t) => {
+    return all.filter(a => a >= t).length - all.filter(a => a <= t).length;
 }
 
 const part2 = (i) => {
-    const result = undefined;
-    return result;
-}
+    //return autoCorrect('[({(<(())[]>[[{[]{<()<>>'.split(''));
+
+    const scores = i
+        .map(autoCorrect)
+    //.filter((d) => d !== 0)
+    //.sort((a, b) => a - b);
+    const diffs = scores.sort((a, b) => countLowerAndHigher(a) - countLowerAndHigher(b));
+    console.log(diffs);
+    return diffs[0];
+    //return scores[Math.ceil(scores.length / 2)];
+};
+
+// not 1217285163
+// not 161510319
+// to low 1546458444
 
 module.exports = {
-    transform, part1, part2, test:1
-}
+    transform,
+    part1,
+    part2,
+    test: 0,
+};
