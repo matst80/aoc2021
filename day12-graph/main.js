ea912b4de6eefea9d811adf591c61d7665514cb2
Mats Törnberg <mats.tornberg@tretton37.com>
2021-12-12 18:29:51 +0100

    graph varaiant on day 12

---
+++ day12-graph/main.js
@@ -0,0 +1,102 @@
+const {
+    seq,
+    log
+} = require("../common.js");
+
+const transform = (input) => {
+    return input
+        .split("\n")
+        .trim()
+        .map((line) => {
+            const [from, to] = line.split("-").trim();
+            return { from, to };
+        });
+};
+
+
+const Graph = (edges, { nodeParser } = {}) => {
+
+    const nodeIds = edges.reduce((all, i) => {
+        const r = [];
+        if (!all.includes(i.from))
+            r.push(i.from);
+        if (!all.includes(i.to))
+            r.push(i.to);
+        return [...all, ...r];
+    }, []);
+
+    const nodes = nodeIds.map(id => {
+        const parents = edges.filter(d => d.to === id).map(d => ({ id: d.from, type: 'parent' }));
+        const children = edges.filter(d => d.from === id).map(d => ({ id: d.to, type: 'child' }));
+        const node = { id, relations: [...children, ...parents] }
+        return nodeParser!==undefined ? nodeParser(node) : node;
+    });
+
+    const getNode = (id) => nodes.find(d => d.id === id);
+
+    const traverse = (node, cb, history = []) => {
+        const state = {};
+        node.relations.forEach(({ id }) => {
+            const relation = getNode(id);
+            const path = [...history, node.id];
+            if (cb(node, relation, path)) {
+                traverse(relation, cb, path);
+            }
+        });
+        return state;
+    }
+    return {
+        getNode,
+        traverse
+    }
+}
+
+const isLower = (a) => a === a.toLowerCase();
+
+const START = 'start';
+
+const defaultOptions = {
+    nodeParser: (node) => {
+        return { ...node, isLarge: !isLower(node.id), edge:node.id===START || node.id==='end' }
+    }
+};
+
+const part2 = (lines) => {
+    const graph = Graph(lines, defaultOptions);
+    let count = 0;
+    
+    graph.traverse(graph.getNode(START), (parent, relation, history) => {
+        
+        if (relation.id === 'end') {
+            count++;
+            return false;
+        }
+        const allSmall = history.filter(isLower);
+        const hasVisitedSmall = allSmall.some(i=>allSmall.filter(j=>j===i).length>1);
+        
+        return !relation.edge && (relation.isLarge || !history.includes(relation.id) || !hasVisitedSmall);
+    });
+    return count;
+};
+
+const part1 = (lines) => {
+    const graph = Graph(lines, defaultOptions);
+    let count = 0;
+    graph.traverse(graph.getNode(START), (parent, relation, history) => {
+        
+        if (relation.id === 'end') {
+            count++;
+            return false;
+        }
+        
+        return !relation.edge && (relation.isLarge || !history.includes(relation.id));
+    });
+    return count;
+};
+
+module.exports = {
+    transform,
+    part1,
+    part2,
+    test: 0,
+};
