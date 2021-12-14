47fb2c70cca8a60b1de4e384a34c5267acb3a35f
Mats Törnberg <mats.tornberg@tretton37.com>
2021-12-14 07:12:31 +0100

    slask

---
+++ day14/main.js
@@ -0,0 +1,17 @@
+const { seq, log, makeGrid, gridLoop, extentArray, numbers, numberGrid, charGrid } = require('../common.js');
+
+const transform = numbers(',');
+
+const part1 = (i) => {
+    const result = i;
+    return result;
+}
+
+const part2 = (i) => {
+    const result = undefined;
+    return result;
+}
+
+module.exports = {
+    transform, part1, part2, test:0
+}
