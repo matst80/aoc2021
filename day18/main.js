7d70eca5940f9c264c1de2af13227d00e4654de5
Mats Törnberg <mats.tornberg@tretton37.com>
2021-12-18 17:34:09 +0100

    slask

--- day18/main.js
+++ day18/main.js
@@ -8,65 +8,69 @@ const magnitude = (pair) => {
 }
 
 const depthCounter = (depth = 0, open = '[', close = ']') => {
-    return [(chr) => {
+    return (chr) => {
+        if (chr === undefined)
+            return depth;
         if (chr === open)
             depth++;
         else if (chr === close)
             depth--;
-    }, () => depth];
+        return depth;
+    };
 }
 
-const getDepth = (line, index) => {
-    const [check, value] = depthCounter();
-    seq(index, line).forEach(check);
-    return value();
-}
-
-const splitLine = (line, index, len = 1) => [line.slice(0, index), line.slice(index + len)];
-
+const splitLine = (line, index, len = 0) => [line.slice(0, index), line.slice(index + len)];
 
 const singleDigit = new RegExp(/(\d+)/);
+const doubleDigits = new RegExp(/\d\d+/g);
+const twoNumbers = new RegExp(/\[(\d+),(\d+)]/g);
 
 const explode = (line, match) => {
-    const [complete, a, b] = match;
+    const [{ length }, a, b] = match;
 
-    const [l, r] = [a, b].map(to => (stringToAdd) => Number(stringToAdd) + Number(to));
+    const [l, r] = [a, b].map(to => toAdd => Number(toAdd) + Number(to));
 
-    let [left, right] = splitLine(line, match.index, complete.length);
+    let [left, right] = splitLine(line, match.index, length);
 
-    return `${left.reverse().replace(singleDigit, l).reverse()}0${right.replace(singleDigit, r)}`;
+    const p1 = left.reverse().replace(singleDigit, l).reverse();
+    const p2 = right.replace(singleDigit, r);
+
+    return `${p1}0${p2}`;
 }
 
 const split = (line, match) => {
-    let [left, right] = splitLine(line, match.index, match[0].length);
+    let [left, right] = splitLine(line, match.index, 2);
 
-    const n = Number(match[0]);
-    return `${left}[${Math.floor(n / 2)},${Math.ceil(n / 2)}]${right}`;
+    const n = Number(match[0]) / 2;
+    return `${left}[${Math.floor(n)},${Math.ceil(n)}]${right}`;
 }
 
 const reduce = (line) => {
-    outer: while (true) {
-        for (const match of line.matchAll(/\[(\d+),(\d+)]/g)) {
-            const depth = getDepth(line, match.index);
-            if (depth >= 4) {
+    loop:
+    while (true) {
+        for (const match of line.matchAll(twoNumbers)) {
+            if (seq(match.index, line).map(depthCounter()).last() >= 4) {
                 line = explode(line, match);
-                continue outer;
+                continue loop;
             }
         }
 
-        for (const match of line.matchAll(/\d\d+/g)) {
+        for (const match of line.matchAll(doubleDigits)) {
             line = split(line, match);
-            continue outer;
+            continue loop;
         }
         break;
     }
+
     return line;
 }
 
 const reduceFn = (acc, line) => reduce(`[${acc},${line}]`);
 
+const magParse = (data) => magnitude(JSON.parse(data));
+
 const part1 = (data) => {
-    return magnitude(JSON.parse(reduce(data.reduce(reduceFn))))
+    return magParse(reduce(data.reduce(reduceFn)));
 }
 
 const part2 = (lines) => {
@@ -75,8 +79,8 @@ const part2 = (lines) => {
         for (let j = i + 1; j < lines.length; j++) {
             max = Math.max(
                 max,
-                magnitude(JSON.parse(reduceFn(lines[i],lines[j]))),
-                magnitude(JSON.parse(reduceFn(lines[j],lines[i])))
+                magParse(reduceFn(lines[i], lines[j])),
+                magParse(reduceFn(lines[j], lines[i]))
             );
         }
     }
